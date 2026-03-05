763c6c2d11828b65cd3b2f6bfde96ff9da684818
650462636292 <153263126+6504062636292@users.noreply.github.com>
2026-03-05 20:28:03 +0700

    Add files via upload

---
+++ lab9.1no4.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void swapValue(int *a, int *b, int *c);
+void swapArray(int a[], int b[], int size);
+
+int main() {
+ 
+    int a = 1, b = 2, c = 3;
+    printf("Before swap function: a=%d, b=%d, c=%d\n", a, b, c);
+
+    swapValue(&a, &b, &c); 
+    
+    printf("After swap function: a=%d, b=%d, c=%d\n", a, b, c);
+
+    printf("\n");
+
+    int size = 3;
+    int arr1[] = {10, 20, 30};
+    int arr2[] = {40, 50, 60};
+
+    printf("Before swapArray:\n");
+    printf("Array 1: %d, %d, %d\n", arr1[0], arr1[1], arr1[2]);
+    printf("Array 2: %d, %d, %d\n", arr2[0], arr2[1], arr2[2]);
+
+    swapArray(arr1, arr2, size);
+
+    printf("After swapArray:\n");
+    printf("Array 1: %d, %d, %d\n", arr1[0], arr1[1], arr1[2]);
+    printf("Array 2: %d, %d, %d\n", arr2[0], arr2[1], arr2[2]);
+
+    return 0;
+}
+
+void swapValue(int *a, int *b, int *c) {
+    int temp = *a;
+    *a = *b;
+    *b = *c;
+    *c = temp;
+}
+
+void swapArray(int a[], int b[], int size) {
+    for (int i = 0; i < size; i++) {
+        int temp = a[i];
+        a[i] = b[i];
+        b[i] = temp;
+    }
+}
